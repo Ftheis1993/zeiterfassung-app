fd021c3531eab71f21fa369631e0d8eb46909d7d
FT <159196850+Ftheis1993@users.noreply.github.com>
2026-07-14 16:29:08 +0200

    Update cache name and maintain service worker functionality

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'protocolapp-cache-vuu';
+const CACHE_NAME = 'protocolavhhhpp-cache-vuu';
 
 // Alle Dateien, die für den echten Offline-Modus geladen werden müssen
 const ASSETS_TO_CACHE = [
