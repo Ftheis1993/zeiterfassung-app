815231da9fae1ad6fcc0ffe497e7e3a34adf1786
FT <159196850+Ftheis1993@users.noreply.github.com>
2026-07-15 11:38:51 +0200

    Update CACHE_NAME and retain caching strategy

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'protocolavhhhpbxxxbhp-caddddbbche-vuu';
+const CACHE_NAME = 'protocolavhhhpbxxxbhhp-caddddbbche-vuu';
 
 // Alle Dateien, die für den echten Offline-Modus geladen werden müssen
 const ASSETS_TO_CACHE = [
